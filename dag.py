dc48556b087e25e44dd709228a1fc29f190e126d
giancarlo habeck <gianhabeck@gmail.com>
2024-09-14 12:06:31 -0300

    fix

--- dag.py
+++ dag.py
@@ -34,25 +34,13 @@ dag = DAG(
     tags=['example']
 )
 
-# spark = open(
-#     "example_spark_kubernetes_operator_pi.yaml").read()
-
 submit = SparkKubernetesOperator(
     task_id='spark_pi_submit',
     namespace="spark",
-    application_file="/opt/airflow/dags/repo/spark_pi.yaml",
+    application_file="./spark_pi.yaml",
     kubernetes_conn_id="kubernetes_default",
     do_xcom_push=True,
     dag=dag,
 )
 
-sensor = SparkKubernetesSensor(
-    task_id='spark_pi_monitor',
-    namespace="spark",
-    application_name="{{ task_instance.xcom_pull(task_ids='spark_pi_submit')['metadata']['name'] }}",
-    kubernetes_conn_id="kubernetes_default",
-    dag=dag,
-    attach_log=True
-)
-
-submit >> sensor
+submit
